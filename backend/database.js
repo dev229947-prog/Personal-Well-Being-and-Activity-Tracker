b3ed805a99f5a3b2e6e8305206eb8a7b35e557e5
dev229947-prog <dev229947@gmail.com>
2026-03-24 20:58:13 +0500

    updated from mongodb'

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,20 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-// In production, use environment variables for credentials!
-const username = encodeURIComponent('dev229947_db_user');
-const password = encodeURIComponent('1KWDiTwo4wHklS3V');
-const uri = `mongodb+srv://${username}:${password}@cluster0.zf5vzlv.mongodb.net/?appName=Cluster0`;
+const username = encodeURIComponent(process.env.MONGODB_USER);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+const cluster = process.env.MONGODB_CLUSTER;
+const uri = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority&appName=Cluster0&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000`;
 
 let client;
 let db;
 
 async function connectDB(dbName = 'test') {
   if (db) return db;
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 10000
+  });
   await client.connect();
   db = client.db(dbName);
   return db;
